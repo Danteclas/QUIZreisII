826aae341c28a0c1afbc7829a17286ef3637189e
Daniel <daniteclas2014@gmail.com>
2023-05-26 07:32:44 -0300

    update quizreis

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,14 +3,14 @@ import { QuizContext } from "../context/quiz";
 
 import "./Welcome.css";
 
-import Quiz from "../img/quiz.svg";
+import Quiz from "../img/QuizREIS.png";
 
 const Welcome = () => {
   const [quizState, dispatch] = useContext(QuizContext);
 
   return (
     <div id="welcome">
-      <h2>Seja bem-vindo</h2>
+      <h2>Bora testar seus conhecimentos?</h2>
       <p>Clique no botão abaixo para começar:</p>
       <button onClick={() => dispatch({ type: "CHANGE_STAGE" })}>
         Iniciar
